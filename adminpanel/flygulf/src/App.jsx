1208b97c366e2dd0cc053328a0c912482b103cfc
PawarSanjana <pawar.sanjana@sargsoftech.com>
2026-02-25 11:14:23 +0530

    did  the  pull   and  resolve the   download  conflicts  in  the  files  and  now  commiting  the  changes

--- adminpanel/flygulf/src/App.jsx
+++ adminpanel/flygulf/src/App.jsx
@@ -1,23 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-     <Router>
+    <Router>
       <Routes>
         {/* Default route → Login */}
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-       
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
-
+export default App;
