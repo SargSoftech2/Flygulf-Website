846a4e53560518633ee4a44bb04879d05d6f7827
Achal Shinde <achal.shinde@sargsoftech.com>
2026-02-27 17:39:56 +0530

    home footer chnages

--- src/app/app.component.routes.ts
+++ src/app/app.component.routes.ts
@@ -6,13 +6,13 @@ import { CoursesComponent } from './courses/courses.component';
 import { DataFlowComponent } from './data-flow/data-flow.component';
 import { ReviewComponent } from './reviews/reviews.component'; // Adjust path as neededimport { BlogsComponent } from './blogs/blogs.component';
 import { ContactusComponent } from './contactus/contactus.component';
-/*import { BlogDetailComponent } from './blog-detail/blog-detail.component';
-import { StepsOfBlsComponent } from './steps-of-bls/steps-of-bls.component';*/
-import { OetConnectorsComponent } from './oet-connectors/oet-connectors.component';
+//import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+//import { StepsOfBlsComponent } from './steps-of-bls/steps-of-bls.component';
+//import { OetConnectorsComponent } from './oet-connectors/oet-connectors.component';
 import { AclsCardiacArrestComponent } from './acls-cardiac-arrest/acls-cardiac-arrest.component';
 import { BlsCourseComponent } from './bls-course/bls-course.component';
 import { SaudiPrometricComponent } from './saudi-prometric/saudi-prometric.component';
-
+//import { BlogsComponent } from './blogs/blogs.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
  { path: 'aboutus', component: AboutComponent }, // Updated
@@ -23,12 +23,13 @@ export const routes: Routes = [
   
   { path: 'contactus', component: ContactusComponent },
   { path: 'reviews', component: ReviewComponent },
-  /*{ path: 'blog/steps-of-bls', component: StepsOfBlsComponent },*/
-  { path: 'blog/oet-connectors', component: OetConnectorsComponent },
+  //{ path: 'blog/steps-of-bls', component: StepsOfBlsComponent },
+ // { path: 'blog/oet-connectors', component: OetConnectorsComponent },
  { path: 'acls', component: AclsCardiacArrestComponent },
  { path: 'saudi-prometric', component: SaudiPrometricComponent },
  // Matches routerLink="/about"
- /*{ path: 'blog/:id', component: BlogDetailComponent },*/
+ //{ path: 'blog/:id', component: BlogDetailComponent },
+//{ path: 'blog', component: BlogsComponent },
   { path: 'contact', component: ContactusComponent },
   { path: 'bls-course', component: BlsCourseComponent },
   { path: '**', redirectTo: '' }
