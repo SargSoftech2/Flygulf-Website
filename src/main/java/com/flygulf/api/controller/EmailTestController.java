8a590bda8db83ae16681cc44875bf6aeea7c5eee
gayatri-1512 <gayatri.patkure@sargsoftech.com>
2026-02-28 10:33:48 +0530

    updated all controller pages

--- src/main/java/com/flygulf/api/controller/EmailTestController.java
+++ src/main/java/com/flygulf/api/controller/EmailTestController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("flygulf/api/test")
 @CrossOrigin(origins = "*")
 public class EmailTestController {
 
--- src/main/java/com/flygulf/api/controller/GalleryController.java
+++ src/main/java/com/flygulf/api/controller/GalleryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/gallery")
+@RequestMapping("flygulf/api/gallery")
 @CrossOrigin(origins = "*")
 public class GalleryController {
 
