4739fb524b6b078f10f0c3702a92bffa40e3b0c8
Rutuja-1203 <rutuja.jagtap@sargsoftech.com>
2026-04-17 16:29:31 +0530

    fix the schedule call redirection

--- src/app/guidance/guidance.component.ts
+++ src/app/guidance/guidance.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // For structural directives
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-guidance',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './guidance.component.html',
   styleUrl: './guidance.component.css',
 })
