f05664f085a60b7b02e925075e5d5ae41d391254
gayatri-1512 <gayatri.patkure@sargsoftech.com>
2026-04-21 11:10:29 +0530

    update

--- src/main/java/com/flygulf/api/repository/CenterRepository.java
+++ src/main/java/com/flygulf/api/repository/CenterRepository.java
@@ -1,7 +1,20 @@
+// package com.flygulf.api.repository;
+
+// import com.flygulf.api.model.Center;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface CenterRepository extends JpaRepository<Center, Long> {
+// }
+
+
+
+
 package com.flygulf.api.repository;
 
 import com.flygulf.api.model.Center;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CenterRepository extends JpaRepository<Center, Long> {
 }
